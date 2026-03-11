bc626d03782a14568b998d121bb25bf7f288a799
Voice of Miyagi <voice-of-miyagi@proton.me>
2026-03-11 21:44:26 +0100

    Create constructors for each contract

--- contracts/security/src/lib.rs
+++ contracts/security/src/lib.rs
@@ -1,8 +1,17 @@
 #![no_std]
-use soroban_sdk::{contract, contractimpl, Env};
+use soroban_sdk::{contract, contractimpl, contracttype, Address, Env};
+
+#[contracttype]
+pub enum DataKey {
+    Admin,
+}
 
 #[contract]
-pub struct Contract;
+pub struct Security;
 
 #[contractimpl]
-impl Contract {}
+impl Security {
+    pub fn __constructor(env: Env, admin: Address) {
+        env.storage().instance().set(&DataKey::Admin, &admin);
+    }
+}
