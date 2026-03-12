13f8288ad28bc3ed4eb2df4f865b1b82290dbf7a
Voice of Miyagi <voice-of-miyagi@proton.me>
2026-03-12 18:36:28 +0100

    Test example of mocking certain authentication, hai!

--- contracts/security/src/tests/counter.rs
+++ contracts/security/src/tests/counter.rs
@@ -1,7 +1,10 @@
 extern crate std;
 
 use super::setup::deploy_contract;
-use soroban_sdk::Env;
+use soroban_sdk::{
+    testutils::{Address as _, MockAuth, MockAuthInvoke},
+    Address, Env, Vec,
+};
 
 #[test]
 fn test_admin_increment() {
@@ -19,13 +22,40 @@ fn test_admin_increment() {
 #[test]
 fn test_assert_admin_auth() {
     let env = Env::default();
-    env.mock_all_auths();
 
     let (client, admin) = deploy_contract(&env);
-    assert_eq!(client.admin(), admin);
-    assert_eq!(client.count(), 0u64);
 
-    // How do I set it up so this is signed by a different user, and thus fails (test only admin can do this)?
+    env.mock_auths(&[MockAuth {
+        address: &admin,
+        invoke: &MockAuthInvoke {
+            contract: &client.address,
+            fn_name: "increment",
+            args: Vec::new(&env),
+            sub_invokes: &[],
+        },
+    }]);
+
     client.increment();
-    // assert_eq!(client.count(), 0u64);
+    assert_eq!(client.count(), 1u64);
+}
+
+#[test]
+#[should_panic(expected = "HostError: Error(Auth, InvalidAction)")]
+fn test_assert_admin_unauthorized() {
+    let env = Env::default();
+
+    let (client, _admin) = deploy_contract(&env);
+    let non_admin = Address::generate(&env);
+
+    env.mock_auths(&[MockAuth {
+        address: &non_admin,
+        invoke: &MockAuthInvoke {
+            contract: &client.address,
+            fn_name: "increment",
+            args: Vec::new(&env),
+            sub_invokes: &[],
+        },
+    }]);
+
+    client.increment();
 }
