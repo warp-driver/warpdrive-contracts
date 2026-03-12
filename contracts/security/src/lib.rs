34194dfff70d9ddce559531d91a4820b748cda98
Voice of Miyagi <voice-of-miyagi@proton.me>
2026-03-12 15:52:03 +0100

    Create separate files for contract's internals and storage sturctures, hai!

--- contracts/security/src/lib.rs
+++ contracts/security/src/lib.rs
@@ -1,42 +1,10 @@
 #![no_std]
-use soroban_sdk::{contract, contractimpl, contracttype, Address, BytesN, Env, String};
 
-#[contracttype]
-pub enum DataKey {
-    Admin,
-    Version,
-}
+mod contract;
+pub mod storage;
 
-#[contract]
-pub struct Security;
-
-#[contractimpl]
-impl Security {
-    pub fn __constructor(env: Env, admin: Address) {
-        env.storage().instance().set(&DataKey::Admin, &admin);
-        env.storage()
-            .instance()
-            .set(&DataKey::Version, &String::from_str(&env, "0.0.1"));
-    }
-
-    pub fn upgrade(env: Env, new_wasm_hash: BytesN<32>, new_version: String) {
-        let admin: Address = env.storage().instance().get(&DataKey::Admin).unwrap();
-        admin.require_auth();
-
-        env.storage()
-            .instance()
-            .set(&DataKey::Version, &new_version);
-        env.deployer().update_current_contract_wasm(new_wasm_hash);
-    }
-
-    pub fn admin(env: Env) -> Address {
-        env.storage().instance().get(&DataKey::Admin).unwrap()
-    }
-
-    pub fn version(env: Env) -> String {
-        env.storage().instance().get(&DataKey::Version).unwrap()
-    }
-}
+pub use contract::Security;
+pub use contract::SecurityClient;
 
 #[cfg(test)]
 mod tests;
