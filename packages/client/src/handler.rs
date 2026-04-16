7edf856f1ee40e6b570e101b4cb6dd75b0bc454d
Merkle Maren <266985194+merkle-maren@users.noreply.github.com>
2026-04-16 09:51:00 +0000

    Start warpdrive-client helpers

---
+++ packages/client/src/handler.rs
@@ -0,0 +1,143 @@
+use soroban_rs::xdr::{
+    ScVal
+};
+use soroban_rs::{
+    Account, ClientContractConfigs, ContractId, Env, EnvConfigs, Signer,
+};
+
+use crate::utils::{execute, query};
+pub struct EthereumHandlerClient {
+    client_configs: soroban_rs::ClientContractConfigs,
+}
+
+impl EthereumHandlerClient {
+    pub fn new(client_configs: &soroban_rs::ClientContractConfigs) -> Self {
+        Self {
+            client_configs: client_configs.clone(),
+        }
+    }
+
+    pub async fn verify_eth(
+        &mut self,
+        envelope_bytes: soroban_rs::xdr::ScVal,
+        sig_data: soroban_rs::xdr::ScVal,
+    ) -> Result<soroban_rs::SorobanTransactionResponse, soroban_rs::SorobanHelperError> {
+        execute(
+            &mut self.client_configs,
+            "verify_eth",
+            vec![envelope_bytes, sig_data],
+        )
+        .await
+    }
+
+    pub async fn upgrade(
+        &mut self,
+        new_wasm_hash: soroban_rs::xdr::ScVal,
+        new_version: soroban_rs::xdr::ScVal,
+    ) -> Result<soroban_rs::SorobanTransactionResponse, soroban_rs::SorobanHelperError> {
+        execute(
+            &mut self.client_configs,
+            "upgrade",
+            vec![new_wasm_hash, new_version],
+        )
+        .await
+    }
+
+    pub async fn propose_admin(
+        &mut self,
+        new_admin: soroban_rs::xdr::ScVal,
+    ) -> Result<soroban_rs::SorobanTransactionResponse, soroban_rs::SorobanHelperError> {
+        execute(&mut self.client_configs, "propose_admin", vec![new_admin]).await
+    }
+
+    pub async fn accept_admin(
+        &mut self,
+    ) -> Result<soroban_rs::SorobanTransactionResponse, soroban_rs::SorobanHelperError> {
+        execute(&mut self.client_configs, "accept_admin", vec![]).await
+    }
+
+    pub async fn admin(&mut self) -> Result<Vec<ScVal>, soroban_rs::SorobanHelperError> {
+        query(&mut self.client_configs, "admin", vec![]).await
+    }
+
+    pub async fn pending_admin(&mut self) -> Result<Vec<ScVal>, soroban_rs::SorobanHelperError> {
+        query(&mut self.client_configs, "pending_admin", vec![]).await
+    }
+
+    pub async fn version(&mut self) -> Result<Vec<ScVal>, soroban_rs::SorobanHelperError> {
+        query(&mut self.client_configs, "version", vec![]).await
+    }
+    pub async fn verification_contract(
+        &mut self,
+    ) -> Result<Vec<ScVal>, soroban_rs::SorobanHelperError> {
+        query(&mut self.client_configs, "verification_contract", vec![]).await
+    }
+    pub async fn payload(
+        &mut self,
+        event_id: soroban_rs::xdr::ScVal,
+    ) -> Result<Vec<ScVal>, soroban_rs::SorobanHelperError> {
+        query(&mut self.client_configs, "payload", vec![event_id]).await
+    }
+}
+
+const MAINNET_PASSPHRASE: &str = "Public Global Stellar Network ; September 2015";
+
+use ed25519_dalek::SigningKey;
+use stellar_strkey::ed25519::PrivateKey;
+
+#[allow(dead_code)]
+pub fn mock_signer1() -> Signer {
+    let pk = PrivateKey::from_string("SD3C2X7WPTUYX4YHL2G34PX75JZ35QJDFKM6SXDLYHWIPOWPIQUXFVLE")
+        .unwrap();
+    Signer::new(SigningKey::from_bytes(&pk.0))
+}
+
+pub async fn demo_query(env_config: EnvConfigs, contract_id: ContractId) {
+    let env = Env::new(env_config).unwrap();
+
+    // TODO: any placeholder for queries (invalid key)
+    let account = Account::single(Signer::new(SigningKey::from_bytes(&[1; 32])));
+
+    let cfg: ClientContractConfigs = ClientContractConfigs {
+        contract_id,
+        env,
+        source_account: account,
+    };
+
+    let mut hc = EthereumHandlerClient::new(&cfg);
+    let ver = hc.version().await.unwrap();
+    println!("{:?}", ver)
+}
+
+pub async fn demo_execute(env_config: EnvConfigs, contract_id: ContractId, account: Account) {
+    let env = Env::new(env_config).unwrap();
+
+    let cfg: ClientContractConfigs = ClientContractConfigs {
+        contract_id,
+        env,
+        source_account: account,
+    };
+
+    let mut hc = EthereumHandlerClient::new(&cfg);
+    let ver = hc.version().await.unwrap();
+    println!("{:?}", ver)
+}
+
+pub async fn demo_main() {
+    let rpc_url = std::env::var("XLM_RPC_URL").unwrap();
+    let contract_str = std::env::var("XLM_CONTRACT").unwrap();
+
+    let contract_id = ContractId::from_string(&contract_str).unwrap();
+
+    let env_config = EnvConfigs {
+        rpc_url: rpc_url.clone(),
+        network_passphrase: MAINNET_PASSPHRASE.to_string(),
+    };
+
+    demo_query(env_config.clone(), contract_id).await;
+
+    // TODO: use real account to sign
+    let account = Account::single(mock_signer1());
+
+    demo_execute(env_config.clone(), contract_id, account).await;
+}
---
+++ packages/client/src/utils.rs
@@ -0,0 +1,101 @@
+use soroban_rs::xdr::{
+    ScVal, SorobanCredentials, Transaction, TransactionEnvelope, TransactionV1Envelope, VecM,
+};
+use soroban_rs::{
+    ClientContractConfigs, Env, Operations,
+    SorobanHelperError, SorobanTransactionResponse, TransactionBuilder,
+};
+
+/// Default transaction fee in stroops (0.00001 XLM)
+pub const DEFAULT_TRANSACTION_FEES: u32 = 100;
+
+// Queries via simulation and returns the ScVal response
+pub async fn query(
+    cfg: &mut ClientContractConfigs,
+    function_name: &str,
+    args: Vec<ScVal>,
+) -> Result<Vec<ScVal>, SorobanHelperError> {
+    let (results, _) = simulate_tx(cfg, function_name, args).await?;
+    Ok(results)
+}
+
+// You can get the ScVal returned from the contract via execute(...).await?.get_return_value()
+pub async fn execute(
+    cfg: &mut ClientContractConfigs,
+    function_name: &str,
+    args: Vec<ScVal>,
+) -> Result<SorobanTransactionResponse, SorobanHelperError> {
+    // Simulate to check failures and calculate proper gas
+    let (_, invoke_tx) = simulate_tx(cfg, function_name, args).await?;
+
+    let env: Env = cfg.env.clone();
+    let tx_envelope = cfg
+        .source_account
+        .sign_transaction(&invoke_tx, &env.network_id())?;
+
+    env.send_transaction(&tx_envelope).await
+}
+
+async fn build_tx(
+    cfg: &mut ClientContractConfigs,
+    function_name: &str,
+    args: Vec<ScVal>,
+) -> Result<Transaction, SorobanHelperError> {
+    let client_configs = cfg;
+
+    let contract_id = client_configs.contract_id;
+    let env: Env = client_configs.env.clone();
+
+    let invoke_operation = Operations::invoke_contract(&contract_id, function_name, args)?;
+
+    TransactionBuilder::new(&client_configs.source_account, &env)
+        .add_operation(invoke_operation)
+        .build()
+        .await
+}
+
+async fn simulate_tx(
+    cfg: &mut ClientContractConfigs,
+    function_name: &str,
+    args: Vec<ScVal>,
+) -> Result<(Vec<ScVal>, Transaction), SorobanHelperError> {
+    let mut tx = build_tx(cfg, function_name, args).await?;
+
+    let tx_envelope = TransactionEnvelope::Tx(TransactionV1Envelope {
+        tx: tx.clone(),
+        signatures: VecM::default(),
+    });
+    let simulation = cfg.env.simulate_transaction(&tx_envelope).await?;
+
+    // Handle error
+    if let Some(err) = simulation.error {
+        return Err(SorobanHelperError::TransactionSimulationFailed(err));
+    }
+
+    let sim_results = simulation.results().unwrap_or_default();
+    // Check for auth issues
+    // TODO: do we need this? taken from soroban-rs "simulate_and_build" but I don't get it
+    for result in &sim_results {
+        for auth in &result.auth {
+            if matches!(auth.credentials, SorobanCredentials::Address(_)) {
+                return Err(SorobanHelperError::NotSupported(
+                    "Address authorization not yet supported".to_string(),
+                ));
+            }
+        }
+    }
+
+    // Determine real fee needed if it will be exeucted
+    let updated_fee = DEFAULT_TRANSACTION_FEES.max(
+        u32::try_from(
+            (tx.operations.len() as u64 * DEFAULT_TRANSACTION_FEES as u64)
+                + simulation.min_resource_fee,
+        )
+        .map_err(|_| SorobanHelperError::InvalidArgument("Transaction fee too high".to_string()))?,
+    );
+    tx.fee = updated_fee;
+
+    // Get results
+    let vals = sim_results.into_iter().map(|r| r.xdr).collect();
+    Ok((vals, tx))
+}
